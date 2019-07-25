8323f96d201100c4a7654dded853ca373d20d892
Twodio <Twodio@DESKTOP-I9A9TGS>
2019-07-25 10:36:42 +0100

    added the command to insert a person depending of an address and an address singularly.

---
+++ src/Dapper.UnitOfWork/Dapper.UnitOfWork.Example/Data/Commands/AddAddressCommand.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dapper.UnitOfWork.Example.Data.Commands
+{
+    public class AddAddressCommand : ICommand, IAsyncCommand
+    {
+        private const string Sql = @"
+            DECLARE @Ident TABLE(n INT);
+			INSERT INTO Addresses(
+				Street,
+				Region)
+            OUTPUT INSERTED.id INTO @Ident(n)
+			VALUES(
+				@Street,
+				@Region); SELECT CAST(n as int) FROM @Ident;
+		";
+
+        private AddressEntity _entity;
+
+        // Set this to true prevents invoking the command without an explicit transaction
+        public bool RequiresTransaction => true;
+
+        public AddAddressCommand(ref AddressEntity entity)
+            => _entity = entity;
+
+        // exeutes the query and assign the returned id to the object we passed in the constructor
+        public void Execute(IDbConnection connection, IDbTransaction transaction)
+            => _entity.Id = connection.ExecuteScalar<int>(Sql, _entity, transaction);
+
+        public Task ExecuteAsync(IDbConnection connection, IDbTransaction transaction, CancellationToken cancellationToken = default)
+            => connection.ExecuteAsync(Sql, _entity, transaction);
+
+        public Task<int> ExecuteAsyncScalar(IDbConnection connection, IDbTransaction transaction, CancellationToken cancellationToken = default)
+            => connection.ExecuteAsync(Sql, _entity, transaction);
+    }
+}
---
+++ src/Dapper.UnitOfWork/Dapper.UnitOfWork.Example/Data/Commands/AddPersonCommand.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dapper.UnitOfWork.Example.Data.Commands
+{
+    public class AddPersonCommand : ICommand, IAsyncCommand
+    {
+        private const string Sql = @"
+            INSERT INTO People(
+				Name,
+				Age,
+                Address_Id)
+			VALUES(
+				@Name,
+				@Age,
+                @Address_Id);
+		";
+
+        private readonly PersonEntity _entity;
+
+        public bool RequiresTransaction => true;
+
+        public AddPersonCommand(PersonEntity entity)
+            => _entity = entity;
+
+        public void Execute(IDbConnection connection, IDbTransaction transaction)
+            => connection.Execute(Sql, new { _entity.Name, _entity.Age, Address_Id = _entity.Address.Id }, transaction);
+
+        public Task ExecuteAsync(IDbConnection connection, IDbTransaction transaction, CancellationToken cancellationToken = default)
+            => connection.ExecuteAsync(Sql, new { _entity.Name, _entity.Age, Address_Id = _entity.Address.Id }, transaction);
+    }
+}
