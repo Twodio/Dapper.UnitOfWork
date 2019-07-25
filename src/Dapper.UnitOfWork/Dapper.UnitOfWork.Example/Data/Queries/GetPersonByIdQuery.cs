1250f9bcca04f5bedff3bbaa4b13d0258fe52f40
Twodio <Twodio@DESKTOP-I9A9TGS>
2019-07-25 10:38:25 +0100

    implemented the Query to fetch people with their respective addresses.

---
+++ src/Dapper.UnitOfWork/Dapper.UnitOfWork.Example/Data/Queries/GetPersonByIdQuery.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dapper.UnitOfWork.Example.Data.Queries
+{
+    public class GetPersonByIdQuery : IQuery<PersonEntity>, IQueryList<PersonEntity>, IAsyncQueryList<PersonEntity>
+    {
+        private const string Sql = "" +
+            "SELECT p.*, a.* FROM People p " +
+                "JOIN Addresses a " +
+                    "ON a.Id = p.Address_Id " +
+            "WHERE p.Id = @Id";
+
+        private int _personId { get; }
+
+        public GetPersonByIdQuery(int personId)
+            => _personId = personId;
+
+        public PersonEntity Execute(IDbConnection connection, IDbTransaction transaction)
+            => connection.Query<PersonEntity, AddressEntity, PersonEntity>(Sql, (person, address) => {
+                person.Address = address;
+                return person;
+            }, new { Id = _personId }, transaction, splitOn: "Id").FirstOrDefault();
+
+        public IEnumerable<PersonEntity> ExecuteList(IDbConnection connection, IDbTransaction transaction)
+            => connection.Query<PersonEntity, AddressEntity, PersonEntity>(Sql, (person, address) => {
+                person.Address = address;
+                return person;
+            }, new { Id = _personId }, transaction, splitOn: "Id");
+
+        public Task<IEnumerable<PersonEntity>> ExecuteAsyncList(IDbConnection connection, IDbTransaction transaction, CancellationToken cancellationToken = default)
+            => connection.QueryAsync<PersonEntity, AddressEntity, PersonEntity>(Sql, (person, address) => {
+                person.Address = address;
+                return person;
+            }, new { Id = _personId }, transaction, splitOn: "Id");
+    }
+}
