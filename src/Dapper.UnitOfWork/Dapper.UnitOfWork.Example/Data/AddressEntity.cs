44ac3fbef64bba73d26ed1c4c4b168804f55c3c4
Twodio <Twodio@DESKTOP-I9A9TGS>
2019-07-25 10:35:04 +0100

    added the Person and Address entities.

---
+++ src/Dapper.UnitOfWork/Dapper.UnitOfWork.Example/Data/AddressEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dapper.UnitOfWork.Example.Data
+{
+    public class AddressEntity
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string Region { get; set; }
+    }
+}
---
+++ src/Dapper.UnitOfWork/Dapper.UnitOfWork.Example/Data/PersonEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dapper.UnitOfWork.Example.Data
+{
+    public class PersonEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Address_Id { get; set; }
+        public AddressEntity Address { get; set; }
+    }
+}
